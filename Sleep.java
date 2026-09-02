d5f6bb5a2d076ad1212a7eca92e9abe921735419
Krish <89413580+Krish-Joshi-byte@users.noreply.github.com>
2026-09-02 15:41:20 -0400

    Update Sleep.java

--- Sleep.java
+++ Sleep.java
@@ -1,1 +1,11 @@
-
+public class Sleep {
+    public static void main(String[] args) {
+        try {
+            System.out.println("Sleeping for 5 seconds...");
+            Thread.sleep(5000); // Sleep for 5 seconds
+            System.out.println("Awake now!");
+        } catch (InterruptedException e) {
+            System.out.println("Sleep was interrupted.");
+        }
+    }
+}
